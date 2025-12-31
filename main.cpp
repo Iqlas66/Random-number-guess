2062770e952d733ec34a79f1d12d21ae7c86483f
iqlas <imiqlas@gmail.com>
2025-12-31 15:48:02 +0100

    MAJOR RELEASE ------------- Features added: •2 game modes: easy and hard

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 
 int main() {
+
+   int input = 0;
+   cout << "Select difficulty:" << endl;
+   cout << "Press 1 for easy" << endl;
+   cout << "Press 2 for hard " << endl;
+   cin >> input;
+   bool easy = (input == 1);
    int attempts = 0;
    srand(time(0));
    int rnd = rand() % 100 + 1;
@@ -19,9 +26,17 @@ int main() {
          cout << "Your prediction is good!" << endl;
          cout << "You got " << 5 - attempts << " points" << endl;
          break;
+      }else if (answer < rnd and easy) {
+         cout << "Your prediction is too small" << endl;
+         cout << "Attempts left: " << (5 - attempts) << endl;
+      }else if (answer > rnd and easy) {
+         cout << "Your prediction is too high" << endl;
+         cout << "Attempts left: " << (5 - attempts) << endl;
+      }else {
+         cout << "Your prediction is bad!" << endl;
+         cout << "Attempts left: " << (5 - attempts) << endl;
       }
-      cout << "Your prediction is bad!" << endl;
-      cout << "Attempts left: " << (5 - attempts) << endl;
+
    }
    if (answer != rnd) {
       cout << "You lose, random number was:  " << rnd  <<endl;
